573ca46d545fc14e935eaf1776963bfd7b571240
AminPriadi <45525407+AminPriadi@users.noreply.github.com>
2019-01-05 10:52:58 +0700

    Add files via upload

---
+++ program menghitung jarak tempuh.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+int main ()
+{
+    int S,V,T;
+    cout<<"masukan kecepatan kendaraan ";
+    cin>>V;
+    cout<<"masukan waktu tempuh kendaraan ";
+    cin>>T;
+
+    S=V*T;
+    cout<<"maka jarak tempuh kendaraan adalah " <<S<<" KM";
+
+
+    return 0;
+}
